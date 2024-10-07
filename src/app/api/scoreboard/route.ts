4cf4521153ab1ba5e6be37b5b162ad0f2d03e38b
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-10-07 18:57:34 +0200

    feat: sorted scoreboard with score

--- src/app/api/scoreboard/route.ts
+++ src/app/api/scoreboard/route.ts
@@ -2,10 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { getCachedContributorsFromTimeFilter } from '@/util/contributors';
 import { getTimeFilterFromAPISearchParam } from '@/util/github';
+import { getSortedContributorsWithScore } from '@/util/score';
 
 export const GET = async (request: NextRequest) => {
   const timeFilter = getTimeFilterFromAPISearchParam(request.nextUrl.searchParams.get('timeFilter'));
   const cachedContributors = await getCachedContributorsFromTimeFilter(timeFilter);
+  const contributorsWithScore = getSortedContributorsWithScore(cachedContributors);
 
-  return NextResponse.json(cachedContributors);
+  return NextResponse.json(contributorsWithScore);
 };
