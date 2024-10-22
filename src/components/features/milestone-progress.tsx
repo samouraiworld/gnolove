6d7612a05a97a067b225f466bbba774e3dafbc33
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-10-22 17:18:24 +0200

    feat: milestone ui

--- src/components/features/milestone-progress.tsx
+++ src/components/features/milestone-progress.tsx
@@ -8,17 +8,18 @@ export interface MilestoneProgressProps {
 }
 
 const MilestoneProgress = ({ milestone, ...props }: MilestoneProgressProps & FlexProps) => {
+  const progress = (milestone.closed_issues / (milestone.open_issues + milestone.closed_issues)) * 100;
+
   return (
     <Flex direction="column" gap="2" {...props}>
       <Flex align="center" justify="center" gap="2">
-        <Text className="italic">{milestone.title}</Text>
+        <Text weight="bold">
+          {milestone.title} ({progress.toFixed(2)}%)
+        </Text>
         <ExternalLinkIcon className="text-blue-10" />
       </Flex>
 
-      <Progress
-        className="w-full"
-        value={(milestone.closed_issues / (milestone.open_issues + milestone.closed_issues)) * 100}
-      />
+      <Progress className="w-full" color="green" value={progress} />
     </Flex>
   );
 };
