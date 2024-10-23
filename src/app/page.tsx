c67f477b8c4788bf98078f834b1e58c3e75c72ea
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-10-23 08:46:41 +0200

    refactor: layout container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import NextLink from 'next/link';
 
-import { Flex, Grid, Heading, ScrollArea, Text } from '@radix-ui/themes';
+import { Grid, Heading, Text } from '@radix-ui/themes';
 
 import MilestoneProgress from '@/feature/milestone-progress';
 import Scoreboard from '@/feature/scoreboard';
 
-import Footer from '@/module/footer';
+import LayoutContainer from '@/layout/layout-container';
+
 import IssuesTable from '@/module/issues-table';
 import UserTable from '@/module/user-table';
 
@@ -54,88 +55,77 @@ const HomePage = async ({ searchParams: { f } }: HomePageParams) => {
   const newContributors = getNewContributors(allTimeCachedContributors, 5);
 
   return (
-    <Flex className="h-screen w-screen" asChild>
-      <ScrollArea>
-        <Flex
-          p={{ initial: '2', sm: '4', lg: '7' }}
-          gap="2"
-          direction="column"
-          className="max-w-screen mx-auto w-full min-w-0 max-w-7xl overflow-hidden"
-        >
-          <Image src={HeaderImage} alt="Header Image" className="rounded-3" />
+    <LayoutContainer>
+      <Image src={HeaderImage} alt="Header Image" className="rounded-3" />
 
-          {milestone && (
-            <NextLink href="/milestone">
-              <MilestoneProgress milestone={milestone} />
-            </NextLink>
-          )}
+      {milestone && (
+        <NextLink href="/milestone">
+          <MilestoneProgress milestone={milestone} />
+        </NextLink>
+      )}
 
-          <Grid columns="3" rows="auto auto" gap="4">
-            <Text weight="bold" size="6" mt="6" asChild>
-              <NextLink href={`https://github.com/${REPOSITORY.owner}/${REPOSITORY.repository}/issues`} target="_blank">
-                👋 Help Wanted!
-              </NextLink>
-            </Text>
+      <Grid columns="3" rows="auto auto" gap="4">
+        <Text weight="bold" size="6" mt="6" asChild>
+          <NextLink href={`https://github.com/${REPOSITORY.owner}/${REPOSITORY.repository}/issues`} target="_blank">
+            👋 Help Wanted!
+          </NextLink>
+        </Text>
 
-            <Text weight="bold" size="6" mt="6">
-              ✔️ Freshly Merged
-            </Text>
+        <Text weight="bold" size="6" mt="6">
+          ✔️ Freshly Merged
+        </Text>
 
-            <Text weight="bold" size="6" mt="6">
-              ⭐ New Rising gnome
-            </Text>
+        <Text weight="bold" size="6" mt="6">
+          ⭐ New Rising gnome
+        </Text>
 
-            <IssuesTable issues={lastIssues} showLabels="on-hover" />
-            <IssuesTable issues={lastMRs} />
-            <UserTable users={newContributors} />
-          </Grid>
+        <IssuesTable issues={lastIssues} showLabels="on-hover" />
+        <IssuesTable issues={lastMRs} />
+        <UserTable users={newContributors} />
+      </Grid>
 
-          <Heading size="6" mt="6" className="text-center">
-            🏅 Gnolove Scoreboard
-          </Heading>
+      <Heading size="6" mt="6" className="text-center">
+        🏅 Gnolove Scoreboard
+      </Heading>
 
-          <Scoreboard contributors={filteredContributors} timeFilter={timeFilter} />
+      <Scoreboard contributors={filteredContributors} timeFilter={timeFilter} />
 
-          <Text weight="bold" size="6" mt="6">
-            🎥 Latest gnoland videos
-          </Text>
+      <Text weight="bold" size="6" mt="6">
+        🎥 Latest gnoland videos
+      </Text>
 
-          <Grid columns={{ initial: '1', xs: '2', md: '3'}} rows="auto" gap="2">
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/-io_Fu7qKrs?si=EjpiUa-fffFmslZx"
-            />
+      <Grid columns={{ initial: '1', xs: '2', md: '3' }} rows="auto" gap="2">
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/-io_Fu7qKrs?si=EjpiUa-fffFmslZx"
+        />
 
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/b3zRbVcJxyE?si=XFl4uW9yt5pj7eYU"
-            />
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/b3zRbVcJxyE?si=XFl4uW9yt5pj7eYU"
+        />
 
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/3czMK3s30KQ?si=Uu9zTyhRNYABOEni"
-            />
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/3czMK3s30KQ?si=Uu9zTyhRNYABOEni"
+        />
 
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/4YUOTt5bDJc?si=VLT1lD9vT4pO2Kt5"
-            />
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/4YUOTt5bDJc?si=VLT1lD9vT4pO2Kt5"
+        />
 
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/ZI0ZGDMbj-U?si=Eu01gxNcbY69y6QU"
-            />
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/ZI0ZGDMbj-U?si=Eu01gxNcbY69y6QU"
+        />
 
-            <YoutubeEmbeddedVideo
-              className="overflow-hidden rounded-4"
-              src="https://www.youtube.com/embed/hTGeG0z09NU?si=Mx4To7XyyJF1rQm2"
-            />
-          </Grid>
-        </Flex>
-
-        <Footer />
-      </ScrollArea>
-    </Flex>
+        <YoutubeEmbeddedVideo
+          className="overflow-hidden rounded-4"
+          src="https://www.youtube.com/embed/hTGeG0z09NU?si=Mx4To7XyyJF1rQm2"
+        />
+      </Grid>
+    </LayoutContainer>
   );
 };
 
---
+++ src/components/layouts/layout-container.tsx
@@ -0,0 +1,24 @@
+import { Flex, FlexProps, ScrollArea } from '@radix-ui/themes';
+
+import Footer from '@/module/footer';
+
+import { cn } from '@/util/style';
+
+const LayoutContainer = ({ className, ...props }: FlexProps) => {
+  return (
+    <Flex className="h-screen w-screen" asChild>
+      <ScrollArea>
+        <Flex
+          p={{ initial: '2', sm: '4', lg: '7' }}
+          gap="2"
+          direction="column"
+          className={cn('max-w-screen mx-auto w-full min-w-0 max-w-7xl overflow-hidden', className)}
+          {...props}
+        />
+
+        <Footer />
+      </ScrollArea>
+    </Flex>
+  );
+};
+export default LayoutContainer;
