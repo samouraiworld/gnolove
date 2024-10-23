be91e26163906b700513017ecbed47ee79e6139d
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-10-23 13:41:14 +0200

    feat: updated the teams

--- src/constants/teams.ts
+++ src/constants/teams.ts
@@ -27,17 +27,36 @@ const TEAMS: Team[] = [
       'moul',
       'leohhhn',
       'dependabot',
-      'villaquiranm',
       'kouteki',
       'kristovatlas',
       'aeddi',
     ],
   },
-  { name: 'Onbloc', color: 'purple', members: ['notJoon', 'r3v4s', 'adr-sk', 'jinoosss'] },
-  { name: 'VarMeta', color: 'yellow', members: ['linhpn99', 'thinhnx-var', 'AnhVAR'] },
-  { name: 'Teritori', color: 'red', members: ['n0izn0iz', 'omarsy', 'villaquiranm', 'hthieu1110', 'MikaelVallenet'] },
-  { name: 'Berty', color: 'green', members: ['jefft0', 'D4ryl00'] },
-  { name: 'DevX', color: 'brown', members: ['ilgooz', 'jeronimoalbi'] },
+  {
+    name: 'Onbloc',
+    color: 'purple',
+    members: ['notJoon', 'r3v4s', 'adr-sk', 'jinoosss'],
+  },
+  {
+    name: 'VarMeta',
+    color: 'yellow',
+    members: ['linhpn99', 'thinhnx-var', 'AnhVAR'],
+  },
+  {
+    name: 'Teritori',
+    color: 'red',
+    members: ['n0izn0iz', 'omarsy', 'villaquiranm', 'hthieu1110', 'MikaelVallenet', 'villaquiranm'],
+  },
+  {
+    name: 'Berty',
+    color: 'green',
+    members: ['jefft0', 'D4ryl00', 'iuricmp'],
+  },
+  {
+    name: 'DevX',
+    color: 'brown',
+    members: ['ilgooz', 'jeronimoalbi'],
+  },
 ];
 
 export default TEAMS;
