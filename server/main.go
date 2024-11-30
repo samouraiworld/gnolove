3708165a8a65bb1c25003456fc4293743ee458c9
Miguel Victoria <miguelvicvil@gmail.com>
2024-11-29 20:53:10 -0500

    finish image

--- server/main.go
+++ server/main.go
@@ -33,6 +33,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	logger.Info("graphqlEndpoint")
 
 	syncer := sync.NewSyncer(database, graphqlEndpoint, repository, owner, logger.Sugar())
 	syncer.StartSynchonizing()
