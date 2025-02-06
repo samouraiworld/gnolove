f709bd65adc9dfacdf529e789ca26787b03cb8b8
Lukas Laudrain <lukas.ldrn@gmail.com>
2025-02-06 17:26:37 +0200

    feat: debug logs

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     indent: ['error', 2, { SwitchCase: 1 }],
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
-    'no-console': ['error', { allow: ['error'] }],
+    'no-console': ['error', { allow: ['error', 'time', 'timeEnd', 'timeLog'] }],
     // 'no-restricted-imports': ['error', { patterns: [{ group: '.*', name: 'No relative imports' }] }],
 
     'linebreak-style': 'off',
