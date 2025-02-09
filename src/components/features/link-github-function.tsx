bf07505095c8312cf794ad2be4238bea04042a6f
Miguel Victoria <miguelvicvil@gmail.com>
2025-02-09 20:09:09 +0100

    set linking after code is post

--- src/components/features/link-github-function.tsx
+++ src/components/features/link-github-function.tsx
@@ -10,24 +10,24 @@ export function useLinkGithub () {
 
   useEffect(() => {
     if (code && !linking) {
-      setLinking(true);
-      processLink(code)
+      processLink(code, setLinking);
     }
   },[code, linking]);
 };
 
 
-function processLink(code:string) {
+function processLink(code:string,setLinking:React.Dispatch<React.SetStateAction<boolean>>) {
     const login = localStorage.getItem("github_login");
     const address = localStorage.getItem("gno_address");
     
     if (login && address) {
-      postCode(code, login, address);
+      postCode(code, login, address)
+      setLinking(true)
     }
   
 }
 
-const postCode = async (code:string, login:string,address:string,) => {
+const postCode = async (code:string, login:string,address:string) => {
   const url = new URL(`/ghtoken?code=${code}&login=${login}&address=${address}`, process.env.NEXT_PUBLIC_API_URL);
   const res = await fetch(url.toString(), { cache: 'no-cache' });
   const data = await res.json();
