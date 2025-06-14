d709ef8bd81f0251129e01f1c4d6911e23d44a16
naim-ea <elayadinaim@gmail.com>
2025-06-14 22:37:43 +0200

    fix: fix styling on error modal

--- src/app/@details/[login]/error.tsx
+++ src/app/@details/[login]/error.tsx
@@ -1,28 +1,15 @@
 'use client';
 
 import { Dialog } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
 
 const ErrorPage = ({ error }: { error: Error & { digest?: string } }) => {
-  const router = useRouter();
-
-  const handleClose = () => {
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push('/');
-    }
-  };
-
   return (
-    <Dialog.Root open onOpenChange={handleClose}>
-      <Dialog.Content>
-        <Dialog.Title>Something went wrong!</Dialog.Title>
-        <div style={{ color: 'red', marginTop: 8 }}>
-          {error?.message || 'Unknown error occurred.'}
-        </div>
-      </Dialog.Content>
-    </Dialog.Root>
+    <>
+      <Dialog.Title>Something went wrong!</Dialog.Title>
+      <Dialog.Description>
+        {error?.message || 'Unknown error occurred.'}
+      </Dialog.Description>
+    </>
   );
 };
 
