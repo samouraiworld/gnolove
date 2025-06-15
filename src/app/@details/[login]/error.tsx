eb8c8295af450fec47a8e6d051ff62486aa0c087
naim-ea <elayadinaim@gmail.com>
2025-06-15 17:28:36 +0200

    feat: add retry button and close button in error dialog

--- src/app/@details/[login]/error.tsx
+++ src/app/@details/[login]/error.tsx
@@ -1,15 +1,30 @@
 'use client';
 
-import { Dialog } from '@radix-ui/themes';
+import { Dialog, Button, IconButton, Flex } from '@radix-ui/themes';
+import { X } from 'lucide-react';
 
-const ErrorPage = ({ error }: { error: Error & { digest?: string } }) => {
+const ErrorPage = ({ error, retry }: { error: Error & { digest?: string }, retry: () => void }) => {
   return (
-    <>
-      <Dialog.Title>Something went wrong!</Dialog.Title>
+    <Flex direction='column'>
+      <Flex justify='between'>
+        <Dialog.Title>Something went wrong!</Dialog.Title>
+        <Dialog.Close>
+          <IconButton variant='ghost' color='gray'>
+            <X size={16} />
+          </IconButton>
+        </Dialog.Close>
+      </Flex>
       <Dialog.Description>
         {error?.message || 'Unknown error occurred.'}
       </Dialog.Description>
-    </>
+      <Button
+        onClick={retry}
+        mt="2"
+        style={{ width: '200px' }}
+      >
+        Retry
+      </Button>
+    </Flex>
   );
 };
 
