d5eba7fe511b34e9fcafb15a92472c7df09175f6
naim-ea <elayadinaim@gmail.com>
2025-06-27 14:47:24 +0200

    fix: improve efficiency of db queries

--- server/models/commit.go
+++ server/models/commit.go
@@ -4,9 +4,9 @@ import "time"
 
 type Commit struct {
 	ID           string    `json:"id" gorm:"primaryKey"`
-	CreatedAt    time.Time `json:"createdAt"`
+	CreatedAt    time.Time `json:"createdAt" gorm:"index:idx_commits_author_created,priority:2"`
 	UpdatedAt    time.Time `json:"updatedAt"`
-	AuthorID     string    `json:"authorID" gorm:"index"`
+	AuthorID     string    `json:"authorID" gorm:"index;index:idx_commits_author_created,priority:1"`
 	Title        string    `json:"title"`
 	URL          string    `json:"URL" `
 	Author       *User     `json:"author"`
