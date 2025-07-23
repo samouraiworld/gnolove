c4d8947b7dce92071533d0f95e5df805a1a78c1e
naim-ea <elayadinaim@gmail.com>
2025-07-23 16:19:43 +0200

    feat: rework cron to use it in Next instead

--- server/main.go
+++ server/main.go
@@ -99,7 +99,6 @@ func main() {
 	router.HandleFunc("/getGithubUserAndTokenByCode", handler.HandleGetGithubUserAndTokenByCode(signer, database))
 	router.HandleFunc("/contributors/{login}", contributor.HandleGetContributor(database))
 	router.Post("/link", handler.HandleLink(database))
-	router.HandleFunc("/discord/notify", handler.HandleDiscordNotification(database))
 
 	// Onchain package contributions endpoints
 	router.HandleFunc("/onchain/packages", handler.HandleGetAllPackages(database))
