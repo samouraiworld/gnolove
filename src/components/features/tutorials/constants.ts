965e652ab09327019661bedb6f6d43c0533a21c6
naim-ea <elayadinaim@gmail.com>
2025-07-23 10:26:28 +0200

    chore: update tutorial videos

--- src/components/features/tutorials/constants.ts
+++ src/components/features/tutorials/constants.ts
@@ -23,4 +23,16 @@ export const TUTORIAL_VIDEOS = [
     src: 'https://www.youtube.com/embed/-Z3M1XVxWU8?si=CuV98ix_ZnjRccpv',
     title: 'Transaction type (Call, Run, Addpkg and Send) - Gno land Education Tutorial',
   },
+  {
+    src: 'https://www.youtube.com/embed/R5t1GmBRUVk?si=3WtMaeFAH_Kg2crq',
+    title: 'Register your User Space - Gno land Education Tutorial',
+  },
+  {
+    src: 'https://www.youtube.com/embed/B7bSreCwIls?si=A-OeUCuh6CXXLT9i',
+    title: 'Deploying Gno Packages - Gno Land Education Tutorial',
+  },
+  {
+    src: 'https://www.youtube.com/embed/-byuFoVYp30?si=GSv1ck2SkAWqfSE6',
+    title: 'Extend your IDE capabilities with Gnopls - Gno Land Education Tutorial',
+  },
 ] as const satisfies ReadonlyArray<{ src: string; title: string }>;
