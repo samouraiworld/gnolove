4a747c1f1da715cbc8a3b65049a1974a034ee7ad
dtczelo <dtczelo@gmail.com>
2025-07-29 10:15:50 +0200

    feat: add pulsating heart loader for page loader proposal

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,19 @@
-import { Flex, Spinner } from '@radix-ui/themes';
+'use client';
+
+import Image from 'next/image';
+
+import { Flex } from '@radix-ui/themes';
+
+import '@/styles/loading.css';
+
+import MinecraftHeart from '@/images/minecraft-heart.png';
 
 const LoadingPage = () => {
   return (
-    <Flex align="center" justify="center" height="100vh" width="100vw">
-      <Spinner />
+    <Flex align="center" justify="center" height="100vh" width="100vw" direction="column" gap="4">
+      <div className="heart-animation">
+        <Image src={MinecraftHeart} alt="Loading Heart" width={36} height={36} />
+      </div>
     </Flex>
   );
 };
