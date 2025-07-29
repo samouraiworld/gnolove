d1ec23018553e806d7fbf867a98ea75b8a185dcd
dtczelo <dtczelo@gmail.com>
2025-07-29 12:15:12 +0200

    chore: move anim in tailwind config && remove use client && make heart smaller

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,18 +1,14 @@
-'use client';
-
 import Image from 'next/image';
 
 import { Flex } from '@radix-ui/themes';
-
-import '@/styles/loading.css';
 
 import MinecraftHeart from '@/images/minecraft-heart.png';
 
 const LoadingPage = () => {
   return (
     <Flex align="center" justify="center" height="100vh" width="100vw" direction="column" gap="4">
-      <div className="heart-animation">
-        <Image src={MinecraftHeart} alt="Loading Heart" width={36} height={36} />
+      <div className="animate-heartPulse">
+        <Image src={MinecraftHeart} alt="Loading Heart" width={28} height={28} />
       </div>
     </Flex>
   );
