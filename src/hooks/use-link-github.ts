d2fc785d5e13f3f07d4a5f8c8551d9e0002a9f11
naim-ea <elayadinaim@gmail.com>
2025-07-31 15:40:16 +0200

    chore: update frontend endpoints

--- src/hooks/use-link-github.ts
+++ src/hooks/use-link-github.ts
@@ -129,7 +129,7 @@ export const useLinkGithub = () => {
 
   const verifyGithubAccount = async (ghToken: string, ghLogin: string, userAddress: string) => {
     const url = new URL(
-      `/verifyGithubAccount?token=${ghToken}&login=${ghLogin}&address=${userAddress}`,
+      `/github/verify?token=${ghToken}&login=${ghLogin}&address=${userAddress}`,
       process.env.NEXT_PUBLIC_API_URL,
     );
     const res = await fetch(url.toString(), { cache: 'no-cache' });
@@ -190,7 +190,7 @@ export const useLinkGithub = () => {
   };
 
   const getGithubUserAndToken = async (code: string) => {
-    const url = new URL(`/getGithubUserAndTokenByCode?code=${code}`, process.env.NEXT_PUBLIC_API_URL);
+    const url = new URL(`/github/oauth/exchange?code=${code}`, process.env.NEXT_PUBLIC_API_URL);
     const res = await fetch(url.toString(), { cache: 'no-cache' });
     const data = await res.json();
 
