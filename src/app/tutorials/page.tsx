0de712e80ffbede69a35df1a7d087b60608a9c7f
Naïm <elayadinaim@gmail.com>
2025-08-20 09:35:18 +0200

    fix: add missing layout container on tutorials page (#94)

--- src/app/tutorials/page.tsx
+++ src/app/tutorials/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import Tutorials from '@/components/features/tutorials/tutorials';
 import { getYoutubePlaylistVideos } from '@/app/actions';
 import { TUTORIAL_VIDEOS_YOUTUBE_PLAYLIST_ID } from '@/features/tutorials/constants';
+import LayoutContainer from '@/layouts/layout-container';
 
 export const metadata: Metadata = {
   title: 'Tutorials and guides',
@@ -10,7 +11,11 @@ export const metadata: Metadata = {
 const TutorialsPage = async () => {
   const videos = await getYoutubePlaylistVideos(TUTORIAL_VIDEOS_YOUTUBE_PLAYLIST_ID);
 
-  return <Tutorials videos={videos} />;
+  return (
+    <LayoutContainer>
+      <Tutorials videos={videos} />
+    </LayoutContainer>
+  );
 };
 
 export default TutorialsPage;
