f80dca45fe32d931cf770befdaf56c2ac0ee9515
naim-ea <elayadinaim@gmail.com>
2025-08-27 14:49:19 +0200

    fix: fix build and deploy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,15 @@
 'use client';
 
-import { Button, Dialog, Flex, IconButton, Text } from '@radix-ui/themes';
-import { X } from 'lucide-react';
-import { useOffline } from '@/contexts/offline-context';
+import { Button, Flex, Heading, Text } from '@radix-ui/themes';
 import Link from 'next/link';
 
 export default function GlobalNotFound() {
-  const { isOffline } = useOffline();
-
   return (
     <Flex direction='column' p='4' gap='3'>
       <Flex justify='between' align='center'>
-        <Dialog.Title>Page not found</Dialog.Title>
-        <IconButton disabled={isOffline} variant='outline' color='gray' size='1'>
-          <X size={16} />
-        </IconButton>
+        <Heading>Page not found</Heading>
       </Flex>
-      <Dialog.Description>
-        <Text size='2' color='gray'>The page you are looking for doesn&apos;t exist or may have been moved.</Text>
-      </Dialog.Description>
+      <Text size='2' color='gray'>The page you are looking for doesn&apos;t exist or may have been moved.</Text>
       <Flex gap='2'>
         <Button asChild>
           <Link href='/'>Go to homepage</Link>
