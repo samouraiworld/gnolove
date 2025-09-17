68390b723cdda601ef35328e5241d6dcadf4ce74
dtczelo <dtczelo@gmail.com>
2025-09-17 16:42:42 +0200

    chore: restore file from main

---
+++ server/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"github.com/samouraiworld/topofgnomes/server/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func InitDB() (*gorm.DB, error) {
+	var err error
+	db, err := gorm.Open(sqlite.Open("/db/database.db"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Commit{},
+		&models.Review{},
+		&models.PullRequest{},
+		&models.Issue{},
+		&models.Milestone{},
+		&models.Repository{},
+		&models.GnoNamespace{},
+		&models.GnoPackage{},
+		&models.GnoProposal{},
+		&models.GnoVote{},
+		&models.File{},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return db, nil
+}
