43894d929406bad44517dcab3d1674373d2590dc
naim-ea <elayadinaim@gmail.com>
2025-09-18 11:08:24 +0200

    feat: trigger cron jobs based on user input

--- server/models/leaderboard_webhook.go
+++ server/models/leaderboard_webhook.go
@@ -1,15 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
 
 type LeaderboardWebhook struct {
-	ID           uint      `gorm:"primarykey;autoIncrement" json:"id"`
-	Url          string    `json:"url"`
-	UserID       string    `json:"userId"`
-	Type         string    `gorm:"column:type;not null;check:type IN ('discord','slack')" json:"type"`
-	Cron         string    `gorm:"column:cron;not null" json:"cron"`
-	Repositories []string  `gorm:"column:repositories;type:text;serializer:json" json:"repositories"`
-	Active       bool      `gorm:"column:active;not null;default:true" json:"active"`
-	CreatedAt    time.Time `gorm:"column:created_at;autoCreateTime" json:"createdAt"`
-	UpdatedAt    time.Time `gorm:"column:updated_at;autoUpdateTime" json:"updatedAt"`
+	ID           uint         `gorm:"primarykey;autoIncrement" json:"id"`
+	Url          string       `json:"url"`
+	UserID       string       `json:"userId"`
+	Type         string       `gorm:"column:type;not null;check:type IN ('discord','slack')" json:"type"`
+	Cron         string       `gorm:"column:cron;not null" json:"cron"`
+	CronId       cron.EntryID `gorm:"column:cron_id;not null" json:"cronId"`
+	Repositories []string     `gorm:"column:repositories;type:text;serializer:json" json:"repositories"`
+	Active       bool         `gorm:"column:active;not null;default:true" json:"active"`
+	CreatedAt    time.Time    `gorm:"column:created_at;autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time    `gorm:"column:updated_at;autoUpdateTime" json:"updatedAt"`
 }
