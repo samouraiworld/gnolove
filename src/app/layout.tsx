e44b75e94cfd45ad64a558f48f014171568f34f0
naim-ea <elayadinaim@gmail.com>
2025-09-30 10:56:48 +0200

    chore: remove redundant meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,7 +137,6 @@ const RootLayout = ({ children, details }: RootLayoutProps) => {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </head>
 
       <body>
