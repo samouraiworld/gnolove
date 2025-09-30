adb5c163a8cec65995663e5b0673dec2e9890a49
Naïm <elayadinaim@gmail.com>
2025-09-30 11:14:06 +0200

    feat: add correct color for pwa topbar (#154)
    * feat: add correct color for pwa topbar
    
    * chore: remove redundant meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,18 @@ export const metadata: Metadata = {
   alternates: {
     canonical: 'https://gnolove.world',
   },
+  
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+  },
+};
+
+export const viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#111113' },
+  ],
 };
 
 interface RootLayoutProps {
@@ -125,7 +137,6 @@ const RootLayout = ({ children, details }: RootLayoutProps) => {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </head>
 
       <body>
