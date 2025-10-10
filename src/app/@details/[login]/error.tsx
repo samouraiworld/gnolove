d75b75169b722074f724ad44fd73e73cf3a32d5c
David <david.gozlan@epitech.eu>
2025-10-10 18:39:34 +0200

    style: pnpm run lint

--- src/app/@details/[login]/error.tsx
+++ src/app/@details/[login]/error.tsx
@@ -2,26 +2,24 @@
 
 import { Dialog, Button, Flex, IconButton } from '@radix-ui/themes';
 import { X } from 'lucide-react';
+
 import { useOffline } from '@/contexts/offline-context';
 
 const ErrorPage = ({ reset }: { reset: () => void }) => {
   const { isOffline } = useOffline();
 
   return (
-    <Flex direction='column' align='center' gap='4'>
-      <Dialog.Close className='absolute top-2 right-4'>
-        <IconButton disabled={isOffline} variant='outline' color='gray' size='1'>
+    <Flex direction="column" align="center" gap="4">
+      <Dialog.Close className="absolute right-4 top-2">
+        <IconButton disabled={isOffline} variant="outline" color="gray" size="1">
           <X size={16} />
         </IconButton>
       </Dialog.Close>
-      <Dialog.Title mb='0'>Something went wrong!</Dialog.Title>
+      <Dialog.Title mb="0">Something went wrong!</Dialog.Title>
       <Dialog.Description>
         We&apos;re sorry, but an unexpected error occurred. Please try again later.
       </Dialog.Description>
-      <Button
-        onClick={reset}
-        style={{ width: '200px' }}
-      >
+      <Button onClick={reset} style={{ width: '200px' }}>
         Try again
       </Button>
     </Flex>
--- src/app/@details/[login]/loading.tsx
+++ src/app/@details/[login]/loading.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import Loader from '@/elements/loader';
 import { Dialog, Flex } from '@radix-ui/themes';
+
+import Loader from '@/elements/loader';
 
 const Loading = () => {
   return (
-    <Flex align='center' gap='2'>
-      <Dialog.Title mb='0'>Loading...</Dialog.Title>
+    <Flex align="center" gap="2">
+      <Dialog.Title mb="0">Loading...</Dialog.Title>
       <Loader width={24} height={24} />
     </Flex>
   );
